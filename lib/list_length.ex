6c33477d12e088473541c4cc4e0ab9780a77f41b
alexandra <alexandrabsouz@gmail.com>
2022-01-07 18:44:11 -0300

    feat: count pattern matching

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,12 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do:  count(list, 0)#Enum.count(list)
+
+  defp count([], acc) do
+    acc
+  end
+
+  defp count([head | tail], acc) do
+    acc = acc + 1
+    count(tail, acc)
   end
 end
